8c887393cd0d0ddeb53440b3d928790b09d878d0
Ahmed Ali <ahmedabolilah.1@gmail.com>
2026-07-16 19:35:06 +0300

    feat: add shopping list exercise for Week3 lists

---
+++ Exercises/Week2/task.py
@@ -0,0 +1,10 @@
+def calculate_area(length, width):
+    return length * width
+result1 = calculate_area(5, 3)
+print(result1)
+result2 = calculate_area(10, 2)
+print(result2) 
+result3 = calculate_area(34,6)
+print(result3)
+result4 = calculate_area(865,2)
+print(result4)
---
+++ Exercises/Week3/shopping_list.py
@@ -0,0 +1,9 @@
+cart = ["milk", "bread", "eggs"]
+
+for item in cart:
+    print(item)
+
+cart.append("cheese")
+cart.pop(0)
+print(cart)
+print("عدد المنتجات في الكارت هو:", len(cart))
